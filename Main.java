d2065f721cb2be072c420c446bac24580879c734
KyotoLand <81872021+KyotoLand@users.noreply.github.com>
2021-04-03 19:42:26 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int i = 1;
+        switch (i){
+            case 1:
+                System.out.println("one");
+            case 2:
+                System.out.println("two");
+                break;
+            default:
+                System.out.println("default");
+        }
+        int s = 0x123F;
+        long x = 3L;
+
+    }
+}
